516cb257c7b9684934e6c6c953afb0687caf27ae
Jeb Feng <ranzeplay@outlook.com>
2023-01-15 22:58:04 +0800

    Add logging

--- src/main/java/me/ranzeplay/instantmarker/server/PositionBroadcast.java
+++ src/main/java/me/ranzeplay/instantmarker/server/PositionBroadcast.java
@@ -1,12 +1,11 @@
 package me.ranzeplay.instantmarker.server;
 
 import me.ranzeplay.instantmarker.BlockBroadcastPacket;
+import me.ranzeplay.instantmarker.InstantMarker;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-// import net.minecraft.sound.SoundCategory;
-// import net.minecraft.sound.SoundEvents;
 
 import static me.ranzeplay.instantmarker.InstantMarker.BROADCAST_LOCATION_ID;
 
@@ -19,8 +18,7 @@ public class PositionBroadcast {
         var packet = new BlockBroadcastPacket(sender.getDisplayName().getString(), buf.readBlockPos()).toPacketByteBuf();
 
         currentWorldPlayers.forEach((player) -> {
-            // sender.getWorld().playSound(null, player.getBlockPos(), SoundEvents.ENTITY_ARROW_HIT_PLAYER, SoundCategory.PLAYERS, 0.5f, 0);
-
+            InstantMarker.LOGGER.debug(player.getName().getString() + " has just broadcast his/her location");
             ServerPlayNetworking.send(player, BROADCAST_LOCATION_ID, packet);
         });
     }
